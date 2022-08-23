4a1eb2f78cfde9bb71419a9135efb0eece3eebac
NikolaiMinbko <60452931+NikolaiMinbko@users.noreply.github.com>
2022-08-24 00:05:12 +0300

    fix filter bug

--- practice-3/scripts/script.js
+++ practice-3/scripts/script.js
@@ -4,26 +4,34 @@ import sort from "./sort.js";
 import search from "./search.js";
 import renderTableHead from "./renderTableHead.js";
 
-const postsList = await fetchPosts();
+const initPostsList = await fetchPosts();
+let postList = initPostsList;
 
 const tableHead = document.querySelector(".table__head");
 const searchInput = document.querySelector(".search__input");
 
-renderTableHead(postsList[0]);
-renderTableData(postsList);
+renderTableHead(postList[0]);
+renderTableData(postList);
 
 tableHead.addEventListener("click", (e) => {
   const sortValue = e.target.innerText;
 
-  sort(postsList, sortValue);
-  renderTableData(postsList);
+  sort(postList, sortValue);
+  renderTableData(postList);
 });
 
 searchInput.addEventListener("input", (e) => {
   const q = e.target.value;
-  if (q.length >= 3 || q.length === 0) {
-    const searchedItems = search(postsList, q);
 
-    renderTableData(searchedItems);
+  if (q.length >= 3) {
+    postList = search(initPostsList, q);
+
+    renderTableData(postList);
+  }
+
+  if (q.length === 0) {
+    postList = initPostsList;
+
+    renderTableData(postList);
   }
 });
